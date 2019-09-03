f13ab01f197709ca83d25bd66dce2660c19a6ee7
Anton <anrehelenius@gmail.com>
2019-09-03 11:56:36 +0200

    made first class

---
+++ HeleniusAnton2002/src/HeleniusAnton2002/test1.java
@@ -0,0 +1,29 @@
+package HeleniusAnton2002;
+import java.util.Scanner;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println(waa(50));
+		System.out.println("Creeper?");
+		
+		String input = scanner.nextLine();
+		
+		System.out.println(input);
+		System.out.println("aww man");
+		
+		if(input == "aww man") {
+			System.out.print("so we back in the mine");
+		}
+	}
+	
+	public static int waa(int a) {
+		return 2*a;
+	}
+
+}
