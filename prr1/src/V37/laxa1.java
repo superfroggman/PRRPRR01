d47a63d379906977c87cd9c1db3ee979bf70a15e
Anton <anrehelenius@gmail.com>
2019-09-10 11:59:27 +0200

    added scanner and input

--- prr1/src/V37/laxa1.java
+++ prr1/src/V37/laxa1.java
@@ -6,6 +6,11 @@ public class laxa1 {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
 		
+		
+		System.out.println("Vad är ditt namn?");
+		
+		String namn = input.nextLine();
 	}
 }
