60782b877e88e4ed60d363b4546738dfc5a953c3
Anton <anrehelenius@gmail.com>
2019-09-10 11:50:43 +0200

    oke actually did code stuff

--- prr1/src/V37/lektion2.java
+++ prr1/src/V37/lektion2.java
@@ -1,10 +1,23 @@
 package V37;
 
+import java.util.Scanner;
+
 public class lektion2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("wa");
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("enter a name:");
+		String a = input.nextLine();
+		
+		System.out.println("alla namn:");
+		String[] namn = {"anton", "linus", "markus"};
+		for(int i=0; i<namn.length;i++) {
+			System.out.println(namn[i]);
+		}
+		System.out.println(a);
 	}
 
 }
