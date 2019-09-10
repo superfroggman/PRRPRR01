a397af6d3b1d5a756ccb2d365b61bd1b7458761e
Anton <anrehelenius@gmail.com>
2019-09-10 12:03:05 +0200

    added remaining questions

--- prr1/src/V37/laxa1.java
+++ prr1/src/V37/laxa1.java
@@ -10,8 +10,21 @@ public class laxa1 {
 		
 		
 		System.out.println("Vad är ditt namn?");
-		
 		String namn = input.nextLine();
+		
+		System.out.println("Vad är din ålder?");
 		String age = input.nextLine();
+		
+		System.out.println("Vad är din adress?");
+		String adress = input.nextLine();
+		
+		System.out.println("Vad är ditt postnummer?");
+		String postnum = input.nextLine();
+		
+		System.out.println("Vilken stad bor du i?");
+		String stad = input.nextLine();
+		
+		System.out.println("Vad är ditt telefonnummer?");
+		String telenum = input.nextLine();
 	}
 }
