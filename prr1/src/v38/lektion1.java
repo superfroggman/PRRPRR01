84e3d60f6642812e145bc29ef35ce23b3a747222
Anton <anrehelenius@gmail.com>
2019-09-16 12:33:53 +0200

    who knows, some code changes maybe

--- prr1/src/v38/lektion1.java
+++ prr1/src/v38/lektion1.java
@@ -1,10 +1,23 @@
 package v38;
 
+import java.util.Scanner;
+
 public class lektion1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("cool cool");
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Hur gammal är du?");
+		int age = input.nextInt();
+
+		if (age >= 18) {
+			System.out.println("Du får köra bil.");
+		} else if (age >= 15) {
+			System.out.println("Du får bara köra moped.");
+		} else {
+			System.out.println("Du får inte köra bil.");
+		}
 	}
 
 }
