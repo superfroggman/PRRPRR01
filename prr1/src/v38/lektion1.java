477aae8f655e20c3e7d71e5a61cdaada277744e6
Anton <anrehelenius@gmail.com>
2019-09-16 13:47:26 +0200

    did stuff

--- prr1/src/v38/lektion1.java
+++ prr1/src/v38/lektion1.java
@@ -7,17 +7,26 @@ public class lektion1 {
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in);
-
-		System.out.println("Hur gammal är du?");
-		int age = input.nextInt();
-
-		if (age >= 18) {
-			System.out.println("Du får köra bil.");
-		} else if (age >= 15) {
-			System.out.println("Du får bara köra moped.");
-		} else {
-			System.out.println("Du får inte köra bil.");
-		}
+		int age = 10;
+		
+		do {
+			System.out.println("Hur gammal är du?");
+			
+			if(!input.hasNextInt()) {
+				System.out.println("Det var inte ett heltal. Du får inte köra något för att du är så elak.");
+				break;
+			}
+			age = input.nextInt();
+			
+			if (age >= 18) {
+				System.out.println("Du får köra bil.");
+			} else if (age >= 15) {
+				System.out.println("Du får bara köra moped.");
+			} else {
+				System.out.println("Du får inte köra bil.");
+			}
+			System.out.println("Fråga igen? (true/false)");
+		} while(input.hasNextBoolean() && input.nextBoolean());
+		System.out.println("Antingen så ville du inte fortsätta eller så skrev du inte true eller false.");
 	}
-
 }
