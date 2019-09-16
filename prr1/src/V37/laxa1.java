61bbe64075ecc1aaf382a5a67d64a353c3b12fc0
Anton <anrehelenius@gmail.com>
2019-09-16 12:00:27 +0200

    Added comments in code

--- prr1/src/V37/laxa1.java
+++ prr1/src/V37/laxa1.java
@@ -6,9 +6,10 @@ public class laxa1 {
 
 	public static void main(String[] args) {
 		
+		//Create input scanner
 		Scanner input = new Scanner(System.in);
 		
-		
+		//Ask for input and save given input
 		System.out.println("Vad är ditt namn?");
 		String namn = input.nextLine();
 		
@@ -27,8 +28,10 @@ public class laxa1 {
 		System.out.println("Vad är ditt telefonnummer?");
 		String telenum = input.nextLine();
 		
+		//Stop scanning for input
 		input.close();
 		
+		//Print input results
 		System.out.println("INFORMATION:\nNamn:\t" + namn + "\nÅlder:\t" + age + "\nAdress:\t" + adress + "\n\t" + postnum + " " + stad + "\nTel:\t" + telenum);
 	}
 }
