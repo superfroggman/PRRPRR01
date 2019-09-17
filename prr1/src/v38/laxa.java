c041a51b4a1532cc75ed55e5a2db18d18c0819ff
Anton <anrehelenius@gmail.com>
2019-09-17 11:59:28 +0200

    added highest and lowest number things

--- prr1/src/v38/laxa.java
+++ prr1/src/v38/laxa.java
@@ -6,17 +6,53 @@ public class laxa {
 
 	public static void main(String[] args) {
 
+		// Initialize input scanner
 		Scanner input = new Scanner(System.in);
-		
+
+		// Create an array of numbers
 		int[] numbers = new int[5];
-		
-		for(int i=0;i<5;i++) {
+
+		// Set array of numbers to user input
+		for (int i = 0; i < 5; i++) {
 			numbers[i] = input.nextInt();
 		}
-		
-		for(int i=0;i<5;i++) {
-			System.out.println(numbers[i]);
-		}
+
+		// Print the result of the function highNumber
+		System.out.println(highNumber(numbers));
+		System.out.println(lowNumber(numbers));
 	}
 
+	public static int highNumber(int[] numbers) {
+
+		// Set maxNumber to first number as standard
+		int maxNum = numbers[0];
+
+		// Loop through numbers and set to max number if larger than previous max
+		// number.
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] > maxNum) {
+				maxNum = numbers[i];
+			}
+		}
+
+		// Return largest number
+		return maxNum;
+	}
+
+	public static int lowNumber(int[] numbers) {
+
+		// Set minNumber to first number as standard
+		int minNum = numbers[0];
+
+		// Loop through numbers and set to min number if larger than previous min
+		// number.
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] < minNum) {
+				minNum = numbers[i];
+			}
+		}
+
+		// Return smallest number
+		return minNum;
+	}
 }
