87420ef7365fb93897b78e9655520560d824988b
Anton <anrehelenius@gmail.com>
2019-09-30 13:09:04 +0200

    stuffffffffffffffff

--- prr1/v40/v40/Primes.java
+++ prr1/v40/v40/Primes.java
@@ -3,10 +3,36 @@ package v40;
 public class Primes {
 
 	public static void main(String[] args) {
-		int[] primes = {1,2};
-		primes[3] = 3;
-		for(int prime : primes) {
+
+		int[] primes = { 2, 3 };
+
+		for (int prime : primes) {
 			System.out.println(prime);
+		}
+
+		int number = 3;
+		while (true) {
+			number++;
+			
+			
+			boolean isPrime = true;
+			for(int prime:primes) {
+				if(number%prime==0) {
+					isPrime = false;
+				}
+			}
+			
+			if(isPrime) {
+				int[] newPrimes = new int[primes.length + 1];
+				int j = 0;
+				for (; j < primes.length; j++) {
+					newPrimes[j] = primes[j];
+				}
+
+				newPrimes[j] = number;
+				primes = newPrimes;
+				System.out.println(number);
+			}
 		}
 	}
 }
