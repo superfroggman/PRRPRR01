22373796d02512417bb5d40868a98fa6fca0e7b0
Anton <anrehelenius@gmail.com>
2019-10-22 12:14:20 +0200

    did much stuff

--- prr1/v43/kattis/ParsingHex.java
+++ prr1/v43/kattis/ParsingHex.java
@@ -9,19 +9,17 @@ public class ParsingHex {
 		Scanner input = new Scanner(System.in);
 
 		boolean zero = false;
-		
+
 		if (true) {
-			System.out.println("niceline!");
 			String line = input.nextLine();
-			
+
 			for (int i = 0; i < line.length(); i++) {
 
 				char sym = line.charAt(i);
 
 				if (zero && (sym == 'x' || sym == 'X')) {
-					hex(line, i);
+					hexLenght(line, i + 1);
 				} else if (sym == '0') {
-					System.out.println(sym + "!!!!!");
 					zero = true;
 				} else {
 					zero = false;
@@ -29,36 +27,74 @@ public class ParsingHex {
 			}
 		}
 
-		//input.close();
+		// input.close();
 	}
+
 	/**
 	 * 
 	 * @param line
 	 * @param pos
 	 */
-	public static void hex(String line, int pos) {
 
-		int sum = 0;
-		
-		System.out.println("hexxx");
-		
-		for (int i = pos; i < line.length(); i++) {
+	static void hexLenght(String line, int pos) {
+
+		int lenght = 0;
+
+		here: for (int i = pos; i < line.length(); i++) {
 			char sym = line.charAt(i);
 
-			int num;
+			if ((sym >= 48 && sym <= 57) || (sym >= 65 && sym <= 70) || (sym >= 97 && sym <= 105)) {
+
+				lenght++;
+			} else {
+				break here;
+			}
+		}
+
+		hexOut(line, pos, lenght);
+		decOut(line, pos, lenght);
+	}
+
+	/**
+	 * 
+	 * @param line
+	 * @param pos
+	 * @param lenght
+	 */
+	public static void hexOut(String line, int pos, int lenght) {
+
+		String hexSum = line.substring(pos, lenght + pos);
+
+		System.out.print(hexSum + " ");
+
+	}
+	
+	/**
+	 * 
+	 * @param line
+	 * @param pos
+	 * @param lenght
+	 */
+	static void decOut(String line, int pos, int lenght) {
+		
+		long decSum = 0L;
+		
+		for (int i = pos; i < pos+lenght; i++) {
+			char sym = line.charAt(i);
+			
+			int num = 0;
 			if ((sym >= 48 && sym <= 57)) {
 				num = sym - 48;
 			} else if ((sym >= 65 && sym <= 70)) {
 				num = sym - 65 + 10;
 			} else if ((sym >= 97 && sym <= 105)) {
 				num = sym - 97 + 10;
-			}else {
-				return;
 			}
 			
-			sum+=num;
+			decSum+=num*Math.pow(16, pos+lenght-i-1);
+			
 		}
-		
-		System.out.println(sum);
+
+		System.out.println(decSum);
 	}
 }
