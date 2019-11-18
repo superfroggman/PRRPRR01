ba0cffe0f319504bb249b4c2c3292dff69472934
superfroggman <49985901+superfroggman@users.noreply.github.com>
2019-11-18 12:17:30 +0100

    Update methodLibrary.java

--- prr1/v46/v46/methodLibrary.java
+++ prr1/v46/v46/methodLibrary.java
@@ -1,60 +1,92 @@
 package v46;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class methodLibrary {
 
     public static void main(String[] args) {
 
-        pq(8,7, true);
+        pq(8, 7, true);
 
+        prim();
     }
 
 
     /**
-     *
      * @param p
      * @param q
      * @param print
      * @return
      */
-    static double[] pq(int p, int q, boolean print){
+    static double[] pq(double p, double q, boolean print) {
         double x1;
         double x2;
-        x1 = -(p/2)+Math.sqrt(Math.pow((p/2), 2)-q);
-        x2 = -(p/2)-Math.sqrt(Math.pow((p/2), 2)-q);
 
-        if(print){
-            System.out.println("x1: "+x1);
-            System.out.println("x2: "+x2);
+        double del1 = -(p / 2);
+        double del2 = Math.sqrt(Math.pow((p / 2), 2) - q);
+        x1 = del1 + del2;
+        x2 = del1 - del2;
+
+        if (print) {
+            System.out.println("x1: " + x1);
+            System.out.println("x2: " + x2);
         }
 
-        return new double[] {x1, x2};
+        return new double[]{x1, x2};
     }
 
+    /**
+     *
+     * @return
+     */
 
-
-    static double prim(){
+    static void prim() {
 
         Scanner input = new Scanner(System.in);
 
-        int[] k = new int[10];
-        int[] e = new int[10];
+        //int[] k = new int[10];
+        ArrayList<Integer> k = new ArrayList<Integer>();
+        ArrayList<Integer> e = new ArrayList<Integer>();
 
+        //get all inputs
         int i = 0;
-        while (true){
+        while(true) {
+
             System.out.println("constant");
-            k[i] = input.nextInt();
+            k.add(input.nextInt());
+
             System.out.println("exponent");
-            e[i] = input.nextInt();
+            e.add(input.nextInt());
 
             System.out.println("press 0 to calculate, 1 to input more");
-            if(input.nextInt()==0) break;
+            if (input.nextInt() == 0) break;
             i++;
         }
 
 
+        //print output
+        System.out.println("Prim: ");
+        for (int j = 0; j < k.size(); j++) {
+            //using the derriveringsregler
+            k.set(j, k.get(j)*e.get(j));
+            e.set(j, e.get(j)-1);
 
-        return 1;
+
+            //print number with exeptions
+            if (e.get(j)==0) {
+                //do nothing here
+            }else if (e.get(j) != 0) {
+                System.out.print(k.get(j) + "X^" + e.get(j));
+            } else {
+                System.out.print(k.get(j));
+            }
+
+            //add + between numbers
+            if(j<k.size()-1 && k.get(j)!=0){
+                System.out.print(" + ");
+            }
+        }
     }
 }
