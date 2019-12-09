86486e222e91de69a7bc1e357b7775d00160f8d2
superfroggman <49985901+superfroggman@users.noreply.github.com>
2019-12-09 12:39:15 +0100

    säkert gjort nåt kanske

---
+++ prr1/v48/v48/Laxa1.java
@@ -0,0 +1,55 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Laxa1 {
+
+    public static void main(String[] args) {
+        // 100 tal i intervallet [0-9]
+        int[] numbers = {1, 1, 1, 2, 6, 7, 8, 4, 3, 7, 8, 9, 3, 1, 3, 7, 8, 5, 3, 4, 8, 9, 6, 4, 2, 4, 7, 9, 7, 4, 3, 2, 3, 6, 7, 8, 7, 7, 5, 7, 9, 6, 1, 4, 0, 8, 6, 5, 6, 8, 9, 0, 7, 5, 4, 3, 2, 4, 5, 9, 8, 5, 9, 8, 8, 4, 5, 6, 7, 8, 9, 0, 9, 0, 9, 7, 5, 2, 1, 2, 3, 4, 5, 4, 4, 5, 3, 4, 5, 0, 8, 7, 0, 7, 9, 7, 0, 6, 5, 4};
+
+
+        // 100 namn
+        String[] names = {"Crystal", "Tam", "Ed", "Beulah", "Daina", "Benjamin", "Kia", "Clelia", "Cassy", "Gita", "Celsa", "Karoline", "Talitha", "Lewis", "Betsy", "Colin", "Glendora", "Carola", "Rosalba", "Jeanie", "Yevette", "Armand", "Neal", "Lilla", "Dorethea", "Delta", "Maye", "Nikita", "Shoshana", "Carola", "Margie", "Haywood", "Venessa", "Natacha", "Gilbert", "Kandi", "Tyisha", "Tammie", "Blossom", "Penney", "Diana", "Audrey", "Willard", "Zoraida", "Drusilla", "Jacquline", "Cyndy", "Janiece", "Tressie", "Kami", "Lashanda", "Leann", "Tom", "Santana", "Junita", "Gisela", "Tom", "Marquerite", "Bryant", "Lauralee", "Yael", "Kelle", "Samantha", "Tom", "Meta", "Lanette", "Wanetta", "Carola", "Jana", "Neal", "Brady", "Rigoberto", "Felicia", "Hellen", "Georgeann", "Carola", "Isaias", "Ellis", "Roseanne", "Lenard", "Ela", "Ophelia", "Alesha", "Mafalda", "Flor", "Kelsi", "Autumn", "Sondra", "Pasty", "Jacquelyne", "Benjamin", "Emmie", "Mickie", "Lang", "Jamee", "Felice", "Daniella", "Carola", "Nathalie", "Genevive"};
+        System.out.println("number of unique names: " + uniqueNames(names));
+    }
+
+
+    public static int uniqueNames(String[] inputNames) {
+
+        ArrayList<String> uniqueNames = new ArrayList<>();
+
+        for (String inputName : inputNames) {
+            boolean unique = true;
+            for (String uniqueName : uniqueNames) {
+                if (inputName.equals(uniqueName)) unique = false;
+            }
+
+            if (unique) {
+                uniqueNames.add(inputName);
+            }
+        }
+
+        return uniqueNames.size();
+    }
+
+    public static int commonName(String[] inputNames) {
+
+        ArrayList<String> uniqueNames = new ArrayList<>();
+        ArrayList<Integer> amount = new ArrayList<>();
+
+
+        for (int i = 0;i<inputNames.length;i++) {
+            boolean unique = true;
+            for (String uniqueName : uniqueNames) {
+                if (inputNames[i].equals(uniqueName)) unique = false;
+            }
+
+            if (unique) {
+				uniqueNames.add(inputNames[i]);
+				amount.add(1);
+			}
+        }
+
+        return uniqueNames.size();
+    }
+}
