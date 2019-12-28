84ffc848aada7beca8a52e12b1c056adaebd019f
anton <anrehelenius@gmail.com>
2019-12-28 18:39:25 +0100

    skrev cloud-remover 4.0

--- prr1/Cloud4/cloud/threadTest.java
+++ prr1/Cloud4/cloud/threadTest.java
@@ -5,8 +5,11 @@ package Cloud;
 public class threadTest extends Thread {
 
     public static void main(String[] args) {
-        threadTest[] t = new threadTest[8];
-        for (int i = 0; i < 8; i++) {
+
+        int threadCount = 16;
+
+        threadTest[] t = new threadTest[16];
+        for (int i = 0; i < 16; i++) {
             t[i] = new threadTest();
             t[i].start();
         }
