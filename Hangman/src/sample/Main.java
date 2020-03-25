19af2ec5fa14445956c2853f686a00ebd9c48dda
Anton <anrehelenius@gmail.com>
2020-03-25 21:00:10 +0100

    eyyyyy

--- Hangman/src/sample/Main.java
+++ Hangman/src/sample/Main.java
@@ -25,6 +25,8 @@ public class Main extends Application {
     private int windowWidth = 1280;
     private int windowHeight = 720;
 
+    private String correctWord = "Nice Word Eyy";
+
     @Override
     public void start(Stage primaryStage) throws Exception{
 
@@ -34,18 +36,21 @@ public class Main extends Application {
         setupIntroScene(primaryStage);
         primaryStage.show();
 
-
-
     }
 
 
+    /**
+     *
+     * @param primaryStage
+     */
     public void setupIntroScene(Stage primaryStage){
 
+        //Setup scene
         introHBox.setBackground(new Background(new BackgroundFill(Color.rgb(51,54,61), CornerRadii.EMPTY, Insets.EMPTY)));
         introScene = new Scene(introHBox, windowWidth, windowHeight);
         primaryStage.setScene(introScene);
 
-
+        //Create button with eventhandler to react when clicked
         Button button1 = new Button("Cool Button");
         button1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -54,22 +59,60 @@ public class Main extends Application {
             }
         });
 
+        //Add button to scene
         introHBox.getChildren().add(button1);
     }
 
 
+    /**
+     *
+     * @param primaryStage
+     */
     public void setupGuessScene(Stage primaryStage){
 
+        //Setup scene
         guessHBox.setBackground(new Background(new BackgroundFill(Color.rgb(0,54,61), CornerRadii.EMPTY, Insets.EMPTY)));
         guessScene = new Scene(guessHBox, windowWidth, windowHeight);
         primaryStage.setScene(guessScene);
 
+        //Setup keylistener
         guessScene.setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
                 System.out.println("Key Pressed: " + keyEvent.getCharacter());
+                makeGuess(keyEvent.getCharacter());
+                System.out.println(getUnderscores(correctWord));
             }
         });
+    }
+
+    /**
+     *
+     * @param guess
+     */
+    public void makeGuess(String guess){
+        System.out.println("is " + guess + " in " + correctWord + "? maybe.");
+    }
+
+    /**
+     *
+     * @param word
+     * @return
+     */
+    public String getUnderscores(String word){
+
+        String out = "";
+
+        //Replaces letters with underscores and spaces with more spaces
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != ' ') {
+                out += "_ ";
+            } else {
+                out += "  ";
+            }
+        }
+
+        return out;
     }
 
 
--- Hangman/src/sample/hangmanBackend.java
+++ Hangman/src/sample/hangmanBackend.java
@@ -20,7 +20,6 @@ public class hangmanBackend {
             } else {
                 showString += "  ";
             }
-
         }
 
         System.out.println(showString);
