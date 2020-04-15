20686ba342fae4aafbb8d7786514b7f8912653de
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-04-15 18:33:21 +0200

    hangman added multiple scenes

--- Hangman/src/sample/Controller.java
+++ Hangman/src/sample/Controller.java
@@ -3,29 +3,47 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 public class Controller {
+
+    @FXML
+    private GridPane mainMenu;
+
+    @FXML
+    private GridPane nextScene;
+
+    @FXML
+    private ComboBox languageSelect;
+
+    @FXML
+    private Text wordText;
+
+
     public void handleSubmitButtonAction(ActionEvent actionEvent) throws IOException {
         System.out.println(languageSelect.getValue());
 
         if (languageSelect.getValue() == "Choose Word") {
             System.out.println("Nice, maybe someday you can actually choose a word");
         } else {
-            System.out.println("The word is: "+ Language.getRandomWord(languageSelect.getValue().toString()+ ".txt"));
-            //wordText.setValue("wow");
+            String word = Language.getRandomWord(languageSelect.getValue().toString()+ ".txt");
+            System.out.println("The word is: "+word);
+            wordText.setText(word);
+
+            mainMenu.setVisible(false);
+            nextScene.setVisible(true);
         }
     }
 
-    @FXML
-    private ComboBox languageSelect;
-    
+
+
 
     public void initialize() {
+        nextScene.setVisible(false);
         fillLanguageSelectBox();
     }
 
